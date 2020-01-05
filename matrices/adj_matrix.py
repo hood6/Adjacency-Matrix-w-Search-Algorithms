12ef9da702df2321907f70f7306d14fd44b96eb8
hood6 <47229201+hood6@users.noreply.github.com>
2020-01-05 18:59:56 -0500

    Added DFS print function
    Added Depth First Search print function to the adjacency matrix as well as testing it in the testing_matrices.py file.
    Known bugs:
         - Last vertex is printed twice, it is not added to the marked list at the end.

--- matrices/adj_matrix.py
+++ matrices/adj_matrix.py
@@ -11,8 +11,17 @@ class AdjMatrix:
             return
         self.matrix = np.zeros((size, size), dtype=np.int32)
         self.vertex_num = 0
+        self.origin_id = 0
         self.size = size
         self.vertices = {}
+        self.marked_list = []
+        self.prev_v = 0
+
+    def ret_vertex(self, v_id):
+        if 0 <= v_id < self.size:
+            print(f"Vertex ID must be [0, {self.size})")
+            return
+        return self.vertices[v_id]
 
     def add_vertex(self):
         if self.vertex_num == self.size:
@@ -23,8 +32,16 @@ class AdjMatrix:
     def add_edge(self, from_vertex, to_vertex, weight=1):
         self.matrix[from_vertex][to_vertex] = weight
 
-    def print_graph(self):
-        return
+    def print_dfs(self):
+        self.marked_list.clear()
+        self.print_dfs_re(self.origin_id)
+
+    def print_dfs_re(self, v_id):
+        print(self.vertices[v_id].to_string())
+        for i in range(len(self.vertices)):
+            if i not in self.marked_list and self.matrix[v_id][i] != 0:
+                self.marked_list.append(v_id)
+                self.print_dfs_re(i)
 
 
 class Vertex:
@@ -39,3 +56,6 @@ class Vertex:
             return
         else:
             self.path = path
+
+    def to_string(self):
+        return f"ID: {self.id} --- Pathable: {self.path}"
