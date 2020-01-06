c8de419b71e7782914867c95c1e6f74ac46383ec
hood6 <47229201+hood6@users.noreply.github.com>
2020-01-06 17:29:15 -0500

    Finish basic Dijkstra's
    Completed the basis of the Dijkstra's algorithm.  As of now it only prints the distances of the nodes from the origin point.  Future updates will include reading the vertices in the graph to see if they are pathable or not, and finding the path to the last vertex

--- pathfinding_algorithms/dijkstras.py
+++ pathfinding_algorithms/dijkstras.py
@@ -3,20 +3,38 @@ from collections import deque
 from sys import maxsize
 
 
-def dijkstras(g, end_id):
+def min_distance(dist_list):
+    min_dist = maxsize
+    for i in dist_list:
+        if i < min_dist:
+            min_dist = i
+    return dist_list.index(i)
+
+
+def dijkstras(g, start_id, end_id):
     if not isinstance(g, AdjMatrix):
         print("Parameter is not a graph.")
         return None
-    distance = [maxsize] * g.size
-    distance[g.origin_id] = 0
-    queue = deque([])
+    distance = {}
+    for i in range(g.size):
+        distance[i] = maxsize
+    distance[start_id] = 0
+    queue = []
     for key in g.vertices:
         queue.append(key)
     visited = []
-    print(queue)
-    print(visited)
     while queue:
-        visited.append(queue.popleft())
-    print(queue)
-    print(visited)
+        check = maxsize
+        for i in queue:
+            if distance[i] < check:
+                check = distance[i]
+                curr_id = i
+        queue.remove(curr_id)
+        visited.append(curr_id)
+        for v_id in g.vertices:
+            if g.matrix[curr_id][v_id] == 1:
+                dist = distance[curr_id] + g.matrix[curr_id][v_id]
+                if dist < distance[v_id]:
+                    distance[v_id] = dist
+    print(distance)
 
