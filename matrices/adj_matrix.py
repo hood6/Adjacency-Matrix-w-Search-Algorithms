6e4dc126f967ffb09cd41e756efe10e1038ca3bd
hood6 <47229201+hood6@users.noreply.github.com>
2020-01-05 21:43:59 -0500

    Documentation and bug fixes
    Documentation has been added to the testing_matrices file and adj_matrix file.  The bug causing the last node to be printed twice has been worked out.

--- matrices/adj_matrix.py
+++ matrices/adj_matrix.py
@@ -3,6 +3,17 @@ import numpy as np
 
 class AdjMatrix:
     def __init__(self, size):
+        """ Initializes adjacency matrix
+
+        Creates a 2D array that represents the edges between the vertices
+        of the graph, as well as a dictionary of all vertices in the
+        graph, the number of vertices. For the print function, the origin
+        and marked vertices list is also initialized.
+
+        @param size: The number of vertices that will be initialized in the
+        graph
+        """
+        # TODO Remove some redundant variables
         if size < 1:
             print("Size of matrix must be greater than zero.")
             return
@@ -18,33 +29,89 @@ class AdjMatrix:
         self.prev_v = 0
 
     def ret_vertex(self, v_id):
+        """ Returns the vertex of the ID passed in
+
+        @param v_id: ID of the vertex being searched for
+        @return: Vertex with the ID that was passed in
+        """
         if 0 <= v_id < self.size:
             print(f"Vertex ID must be [0, {self.size})")
             return
         return self.vertices[v_id]
 
     def add_vertex(self):
+        """ Adds a vertex to the graph
+
+        Checks to make sure that the vertex being added will
+        not exceed the size of the graph. If it is not, then it
+        is added to the dictionary with the key being the ID of
+        the vertex being added. The vertex_num variable is increased
+        by 1. Each vertex created is by default pathable.
+
+        """
         if self.vertex_num == self.size:
             return
         self.vertices[self.vertex_num] = Vertex(self.vertex_num, True)
         self.vertex_num += 1
 
     def add_edge(self, from_vertex, to_vertex, weight=1):
+        """ Adds an edge to the matrix
+
+        Adds a weight to the matrix to represent an edge between
+        two vertices. By default, the weight is 1 if no weight is
+        passed in. This is compatible with undirectional and
+        directional graphs.
+
+        @param from_vertex: The vertex that the edge comes from
+        @param to_vertex: The vertex that the edge goes to
+        @param weight: The value of the edge, represented by an
+        integer
+        """
+        # TODO Makes weights compatible with float values ranging from 0 to 1
         self.matrix[from_vertex][to_vertex] = weight
 
     def print_dfs(self):
+        """ Uses the Depth First Search algorithm to print the graph
+
+        This function is called to print the graph using a depth first
+        search algorithm. After ensuring that the marked vertex list is
+        cleared, this function calls on the print_dfs_re utility function
+        to perform most of the work and passes the origin of the graph to
+        it.
+
+        """
         self.marked_list.clear()
         self.print_dfs_re(self.origin_id)
 
     def print_dfs_re(self, v_id):
+        """ Called on by the print_dfs function to print the graph
+
+        This utility function makes use of depth first search to print
+        the graph to the console. Initially the function checks the
+        edges on the matrix of the ID passed in. If it comes upon an edge
+        that does not lead to a marked vertex, the current ID is appended
+        to the list, and the ID that the edge leads to is passed into the
+        function recursively.
+
+        @param v_id: The vertex ID that represents the 'from' vertex
+        """
         print(self.vertices[v_id].to_string())
         for i in range(len(self.vertices)):
             if i not in self.marked_list and self.matrix[v_id][i] != 0:
+                if i == self.vertex_num-1:
+                    self.marked_list.append(i)
                 self.marked_list.append(v_id)
                 self.print_dfs_re(i)
 
 
 class Vertex:
+    """ Framework for the vertices in the graph
+
+    The Vertex objects hold a unique ID number as well
+    as a boolean value that indicates whether the node is
+    pathable or not.
+
+    """
     def __init__(self, v_id, path):
         if not isinstance(v_id, int):
             print("Given ID is not an integer.")
@@ -59,3 +126,12 @@ class Vertex:
 
     def to_string(self):
         return f"ID: {self.id} --- Pathable: {self.path}"
+
+
+"""
+Notes:
+    - For pathfinding algorithms, I could also just have vertices
+        with no edges to represent obstacles rather than a
+        boolean that determines whether or not the vertex is pathable
+        or not.
+"""
--- testing_matrices.py
+++ testing_matrices.py
@@ -1,27 +1,48 @@
 from matrices.adj_matrix import AdjMatrix
-from math import sqrt
 
-arr_size = 9
-arr_length = int(sqrt(arr_size))
+arr_length = 3  # Length of the square graph
+arr_size = arr_length*arr_length  # Total number of nodes in the graph
 arr_west_edge = arr_length-1
 mat = AdjMatrix(arr_size)
-for i in range(arr_size):
+for i in range(arr_size):  # Adds vertices to the graph
     mat.add_vertex()
-for i in range(arr_size):
-    north_id = i-arr_length
-    south_id = i+arr_length
-    east_id = i+1
-    west_id = i-1
+for i in range(arr_size):  # Adds edges to the matrix
+    north_id = i-arr_length  # Potential ID of the node above the current node
+    south_id = i+arr_length  # Potential ID of the node below the current node
+    east_id = i+1  # Potential ID of the node to the right of the current node
+    west_id = i-1  # Potential ID of the node to the left of the current node
+    """
+    Checks the potential ID of the node above the current node to make sure
+    that it is within the bounds of the graph. If the statement returns false,
+    no edge is added
+    """
     if north_id >= 0:
         mat.add_edge(from_vertex=i, to_vertex=north_id)
         mat.add_edge(from_vertex=north_id, to_vertex=i)
+    """
+    Checks the potential ID of the node below the current node to make sure
+    that it is within the bounds of the graph. If the statement returns false,
+    meaning that the ID is greater than the length of the graph, no edge is added.
+    """
     if south_id < arr_length:
         mat.add_edge(from_vertex=i, to_vertex=south_id)
         mat.add_edge(from_vertex=south_id, to_vertex=i)
+    """
+    Checks the potential ID of the node to the right of the current node. This
+    check ensures that a node on the right side of the graph does not connect
+    to the next node on the left side and that the ID is less than the size of
+    the graph. If either statement returns false, no edge is added.
+    """
     if east_id % arr_length != 0 and east_id < arr_size:
         mat.add_edge(from_vertex=i, to_vertex=east_id)
         mat.add_edge(from_vertex=east_id, to_vertex=i)
+    """
+    Checks the potential ID of the node to the left of the current node. This
+    check ensures that a node on the left side of the graph does not connect to
+    the previous node on the right side and that the ID is not less than 0. If
+    either statement returns false, no edge is added.
+    """
     if west_id % arr_length != arr_west_edge and west_id >= 0:
         mat.add_edge(from_vertex=i, to_vertex=west_id)
         mat.add_edge(from_vertex=west_id, to_vertex=i)
-mat.print_dfs()
+mat.print_dfs() # Prints the contents of the graph using Depth first search
